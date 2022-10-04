47b4a68193d84d60ec324582a67236debcbcef89
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 04:02:14 +0000

    Adding numeric_owner as keyword arguement

--- vim/bundle/YouCompleteMe/third_party/ycmd/build.py
+++ vim/bundle/YouCompleteMe/third_party/ycmd/build.py
@@ -777,7 +777,7 @@ def ExtractCsCompleter( writeStdout, build_dir, package_path ):
               if not is_within_directory(path, member_path):
                   raise Exception("Attempted Path Traversal in Tar File")
       
-          tar.extractall(path, members, numeric_owner) 
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
           
       
       safe_extract(package_tar)
@@ -1027,7 +1027,7 @@ def EnableJavaCompleter( switches ):
             if not is_within_directory(path, member_path):
                 raise Exception("Attempted Path Traversal in Tar File")
     
-        tar.extractall(path, members, numeric_owner) 
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
         
     
     safe_extract(package_tar, REPOSITORY)
@@ -1128,7 +1128,7 @@ def DownloadClangd( printer ):
             if not is_within_directory(path, member_path):
                 raise Exception("Attempted Path Traversal in Tar File")
     
-        tar.extractall(path, members, numeric_owner) 
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
         
     
     safe_extract(package_tar, CLANGD_OUTPUT_DIR)
--- vim/bundle/YouCompleteMe/third_party/ycmd/update_boost.py
+++ vim/bundle/YouCompleteMe/third_party/ycmd/update_boost.py
@@ -107,7 +107,7 @@ def Extract( path, folder = os.curdir ):
             if not is_within_directory(path, member_path):
                 raise Exception("Attempted Path Traversal in Tar File")
     
-        tar.extractall(path, members, numeric_owner) 
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
         
     
     safe_extract(f, folder)
